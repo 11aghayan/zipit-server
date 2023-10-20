58b5079895f7e2d6b1b3fa9d8726d15af2a86676
Garik Aghayan <11.aghayan.99@gmail.com>
2023-10-20 18:33:48 +0400

    controllers: getItems_admin deleted;

--- src/controllers/getItems_admin.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export default function async (req: Request, res: Response) {
-
-}
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,12 +3,9 @@ import addCategory from "./category_controllers/addCategory";
 import editCategory from "./category_controllers/editCategory"; 
 import deleteCategory from './category_controllers/deleteCategory';
 
-import getItems_admin from "./getItems_admin";
-
 export {
   getCategories_admin,
   addCategory,
   editCategory,
-  deleteCategory,
-  getItems_admin,
+  deleteCategory
 };
