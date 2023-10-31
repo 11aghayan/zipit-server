c21eeb014ea72f6c4ea95d5f5bd6505059a52369
Garik Aghayan <11.aghayan.99@gmail.com>
2023-10-31 12:59:18 +0400

    console.log deleted from credtentials

--- src/middleware/credentials.ts
+++ src/middleware/credentials.ts
@@ -3,7 +3,7 @@ import { allowedOrigins } from "../config/corsOptions";
 
 export default function (req: Request, res: Response, next: NextFunction) {
   const { origin } = req.headers;
-  console.log(origin);
+  
   if (origin && allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Credentials', 'true');
   }
