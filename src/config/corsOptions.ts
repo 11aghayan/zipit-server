dd2187696b3cb56aacd06f7bbbcd7330f14985a3
Garik Aghayan <11.aghayan.99@gmail.com>
2023-10-31 13:47:19 +0400

    vercel.json modified

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,9 +1,9 @@
 export const allowedOrigins = [
   'https://zipit.am',
-  'https://zipit-admin.vercel.app/',
-  'https://zipit-admin-11aghayan.vercel.app/',
-  'https://zipit-admin-git-main-11aghayan.vercel.app/',
-  'https://zipit-admin-8d880qzk5-11aghayan.vercel.app/'
+  'https://zipit-admin.vercel.app',
+  'https://zipit-admin-11aghayan.vercel.app',
+  'https://zipit-admin-git-main-11aghayan.vercel.app',
+  'https://zipit-admin-8d880qzk5-11aghayan.vercel.app'
 ];
 
 export default {
