d1b1283ed70dfd0de96b36b57fffa9032bed186d
Garik Aghayan <11.aghayan.99@gmail.com>
2023-10-31 15:04:34 +0400

    unnecessary imports deleted

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
