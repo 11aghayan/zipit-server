11a36aa96776c77d888632eb5f285e01df9809ff
Garik Aghayan <11.aghayan.99@gmail.com>
2024-05-03 00:24:27 +0400

    cors changed

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -1,20 +1,21 @@
 import cors from 'cors';
 
 export const allowedOrigins = [
-  'https://zipit.am',
   'https://zipit-admin.onrender.com',
+  'https://zipit.am',
+  'https://zipit-public.onrender.com',
   'http://localhost:5173',
   'http://localhost:4173'
 ];
 
 export const publicCors = cors({
-  origin: [allowedOrigins[0], allowedOrigins[2], allowedOrigins[3]],
+  origin: [allowedOrigins[1], allowedOrigins[2], allowedOrigins[3], allowedOrigins[4]],
   methods: ['GET'],
   allowedHeaders: ['Content-Type', 'Authorization']
 });
 
 export const adminCors = cors({
-  origin: allowedOrigins[1],
+  origin: allowedOrigins[0],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
