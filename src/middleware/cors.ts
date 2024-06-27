e78c9888420c7727ddb52a7b82c4cf43d3467cfe
Garik Aghayan <11.aghayan.99@gmail.com>
2024-06-27 18:08:50 +0400

    cors changed

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,9 +4,9 @@ import { adminCors, publicCors } from '../config/corsOptions';
 
 export default function(req: Request, res: Response, next: NextFunction) {
   const origin = req.get('origin');
-  if (origin === 'https://zipit-admin.onrender.com' || origin === 'http://localhost:5173') {
+  if (origin === 'https://zipit-admin.onrender.com') {
     adminCors(req, res, next);
-  } else if (origin === 'https://zipit.am' || origin === 'https://zipit-public.onrender.com' || origin === 'http://localhost:5173' || origin === 'http:localhost:4173') {
+  } else if (origin === 'https://zipit.am' || origin === 'https://zipit-public.onrender.com') {
     publicCors(req, res, next);
   } else {
     // Handle other origins or no origin header as needed
