7dc511af300b3ed6e5fdbdd4c5d030de03a50a80
11aghayan <11.aghayan.99@gmail.com>
2024-08-15 00:24:01 +0400

    no available size handled

--- src/utils/getFirstAvailableColorAndSize.ts
+++ src/utils/getFirstAvailableColorAndSize.ts
@@ -1,4 +1,4 @@
-import { LanguageType } from "../types";
+import { LanguageType, PhotoType } from "../types";
 
 type ItemType = {
   size: {
@@ -6,13 +6,17 @@ type ItemType = {
       colors: string[] ,
       value: number;
     }[]
-  }
+  },
+  photos: {
+    color: string;
+    src: string;
+  }[];
 }
 
 export default function getFirstAvailableColorAndSize(item: ItemType, lang: LanguageType) {
-  const size = item.size.values.find(val => val.colors.length > 0)!;
+  const size = item.size.values.find(val => val.colors.length > 0);
 
-  const color = size.colors[0].split('&dash&')[lang === 'am' ? 0 : 1];
+  const color = size?.colors[0]?.split('&dash&')[lang === 'am' ? 0 : 1] || item.photos[0].color;
   
-  return {color, size: size.value};
+  return {color, size: size?.value || null};
 }
